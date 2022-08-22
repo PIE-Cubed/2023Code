b83f695f8abc3e037fef49f69d95c108af8ade0e
Charipi <cburometto3@gmail.com>
2022-08-22 12:32:01 -0400

    Created base code for driving

---
+++ 2023 Competition Code/src/main/java/frc/robot/Controls.java
@@ -0,0 +1,124 @@
+package frc.robot;
+
+/**
+ * Imports
+ */
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+
+/**
+ * Start of class
+ */
+public class Controls {
+    // CONSTANTS
+    private final int JOYSTICK_ID = 1;
+    private final int XBOX_ID     = 0;
+
+    // Controller object declaration
+    private Joystick       joystick;
+    private XboxController xboxController;
+
+    // Rate limiters
+    private SlewRateLimiter xLimiter;
+
+    //Constructor
+    public Controls() {
+        // Instance Creation
+        joystick       = new Joystick(JOYSTICK_ID);
+        xboxController = new XboxController(XBOX_ID);
+
+        // Rate limiter
+        xLimiter = new SlewRateLimiter(0.75);
+    }
+
+    /**
+     * JOYSTICK FUNCTIONS
+     */
+    /**
+     * DRIVE FUNCTIONS
+     */
+    /**
+     * Positive values are from clockwise rotation and negative values are from counter-clockwise
+     * @return rotatePower
+     */
+    public double getRotatePower() {
+        double power = joystick.getZ(); 
+
+        //If we are in deadzone or strafelock is on, rotatepower is 0
+        if ((Math.abs(power) < 0.3) || (getStrafeLock() == true)) {
+            power = 0;
+        }
+
+        //Cubes the power and clamps it because the rotate is SUPER sensitive
+        power = Math.pow(power, 3.0);
+        power = MathUtil.clamp(power, -.5, .5);
+            
+        return power;    
+    }
+
+    /**
+     * Gets the drive X
+     * @return driveX
+     */
+    public double getDriveX() {
+        double power = joystick.getX();
+
+        //Strafe lock removes deadzone and cubes power for more precision
+        if (getStrafeLock() == true) {
+            power = Math.pow(power, 3);
+        }
+        //If we are in deadzone or rotatelock is on, x is 0
+        if ((Math.abs(power) < 0.05) || (getRotateLock() == true)) {
+            power = 0;
+        }
+
+        //Prevents us from accelerating sideways too quickly
+        power = xLimiter.calculate(power);
+ 
+        return power;
+    }
+
+    /**
+     * Gets the drive Y
+     * @return driveY
+     */
+    public double getDriveY() {
+        double power = joystick.getY() * -1;
+
+        //Strafe lock removes deadzone and cubes power for more precision
+        if (getStrafeLock() == true) {
+            power = Math.pow(power, 3);
+        }
+        //If we are in deadzone or rotatelock is on, y is 0
+        else if ((Math.abs(power) < 0.05) || (getRotateLock() == true)) {
+            power = 0;
+        }
+        return power;
+    }
+
+    /**
+     * Checks if we are in strafe lock mode
+     * @return joystick button 5
+     */
+    private boolean getStrafeLock() {
+        return joystick.getRawButton(5);
+    }
+
+    /**
+     * Checks if we are in rotate lock mode
+     * @return joystick button 3
+     */
+    private boolean getRotateLock() {
+        return joystick.getRawButton(3);
+    }
+
+    /*
+    public boolean toggleFieldDrive() {
+        return joystick.getRawButton(10);
+    }*/
+}
+
+// End of the Controls class
---
+++ 2023 Competition Code/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,190 @@
+package frc.robot;
+
+/**
+ * Imports
+ */
+import edu.wpi.first.wpilibj.TimedRobot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The class that runs eveything else
+ */
+public class Robot extends TimedRobot {
+  // ERROR CODES
+  public static final int FAIL = -1;
+  public static final int PASS =  1;
+  public static final int DONE =  2;
+  public static final int CONT =  3;
+
+  // Object creation
+  Drive    drive;
+  Controls controls;
+
+  // Variables
+  private int status = CONT;
+
+  // Enumeration for manual or automatic control
+  public static enum DriveMode {
+    MANUAL;
+  }
+  private DriveMode driveMode = DriveMode.MANUAL;
+
+  // Auto path
+  private static final String kCenterAuto = "Center";
+  private static final String kWallAuto   = "Wall";
+  private static final String kHangarAuto = "Hangar";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  // Auto Delay
+  private int delaySec = 0;
+
+  /**
+   * Constructor
+   */
+  public Robot() {
+    // Instance creation
+    drive    = new Drive();
+    controls = new Controls();
+  }
+
+  @Override
+  /**
+   * robotInit()
+   * Runs once when the robot is started
+   */
+  public void robotInit() {
+    // Auto start location
+    m_chooser.setDefaultOption("Wall Auto", kWallAuto);
+    m_chooser.addOption("Center Auto", kCenterAuto);
+    m_chooser.addOption("Hangar Auto", kHangarAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+
+    // Auto delay
+    SmartDashboard.putNumber("Auto delay seconds", 0);
+  }
+
+  @Override
+  /**
+   * robotPeriodic()
+   * Always runs on the robot
+   */
+  public void robotPeriodic() {
+  }
+
+  @Override
+  /**
+   * autonomousInit()
+   * Runs once when Auto starts
+   */
+  public void autonomousInit() {
+    // Choses start position
+    m_autoSelected = m_chooser.getSelected();
+    System.out.println("Auto selected: " + m_autoSelected);
+
+    delaySec = (int)SmartDashboard.getNumber("Auto delay seconds", 0);
+
+    // Resets the gyro
+    Drive.ahrs.zeroYaw();
+  }
+
+  @Override
+  /**
+   * autonomousPeriodic()
+   * Runs constantly during Autonomous
+   */
+  public void autonomousPeriodic() {
+    long autoDelayMSec = delaySec * 1000;
+
+    if (status == Robot.CONT) {
+      switch (m_autoSelected) {
+        default:
+          status = DONE;
+          break;
+      }
+    }
+  }
+
+  @Override
+  /**
+   * teleopInit()
+   * Runs once at the start of TeleOp
+   */
+  public void teleopInit() {
+  }
+
+  @Override
+  /**
+   * teleopPeriodic()
+   * Runs constantly during TeleOp
+   */
+  public void teleopPeriodic() {
+    wheelControl();
+  }
+
+  @Override
+  /**
+   * disabledInit()
+   */
+  public void disabledInit() {    
+  }
+
+  @Override
+  /**
+   * disabledPeriodic()
+   * Shouldn't do anything
+   */
+  public void disabledPeriodic() {
+    // Nothing yet...
+  }
+
+  @Override
+  /**
+   * testInit()
+   * Runs once at the start of Test
+   */
+  public void testInit() {
+    // Resets status
+    status = Robot.CONT;
+  }
+
+  @Override
+  /**
+   * testPeriodic()
+   * Runs constantly during test
+   */
+  public void testPeriodic() {
+  }
+
+  /**
+   * Controls the wheels in TeleOp
+   */
+  private void wheelControl() {
+    // Gets Joystick Values
+    double driveX               = controls.getDriveX();
+    double driveY               = controls.getDriveY();
+    double rotatePower          = controls.getRotatePower();
+
+    // Manual driving
+    if (driveMode == DriveMode.MANUAL) {
+      // Drives if we are out of dead zone
+      if ((Math.abs(driveX) > 0.05) ||
+          (Math.abs(driveY) > 0.05) || 
+          (Math.abs(rotatePower) > 0.01)) {
+        drive.teleopSwerve(driveX, driveY, rotatePower, false, true);
+      }
+      else {
+        // Robot is in dead zone, doesn't drive
+        drive.stopWheels();
+      }
+    } 
+
+    // Limelight targeting
+    else {
+      //
+    }
+  }
+}
+// End of the Robot class
