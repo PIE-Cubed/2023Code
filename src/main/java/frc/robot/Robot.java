3e780550f9f4953c00c4f8b0dd3b69934d4069b4
Alex Pereira <alex.pereira.6464@gmail.com>
2022-12-23 21:49:45 -0500

    Command Based Programming Imported the Command Based Robot Template
    Signed-off-by: Alex Pereira <alex.pereira.6464@gmail.com>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,190 +1,95 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-/**
- * Imports
- */
 import edu.wpi.first.wpilibj.TimedRobot;
-
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
- * The class that runs eveything else
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
  */
 public class Robot extends TimedRobot {
-  // ERROR CODES
-  public static final int FAIL = -1;
-  public static final int PASS =  1;
-  public static final int DONE =  2;
-  public static final int CONT =  3;
+  private Command m_autonomousCommand;
 
-  // Object creation
-  Drive    drive;
-  Controls controls;
-
-  // Variables
-  private int status = CONT;
-
-  // Enumeration for manual or automatic control
-  public static enum DriveMode {
-    MANUAL;
-  }
-  private DriveMode driveMode = DriveMode.MANUAL;
-
-  // Auto path
-  private static final String kCenterAuto = "Center";
-  private static final String kWallAuto   = "Wall";
-  private static final String kHangarAuto = "Hangar";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
-
-  // Auto Delay
-  private int delaySec = 0;
+  private RobotContainer m_robotContainer;
 
   /**
-   * Constructor
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
    */
-  public Robot() {
-    // Instance creation
-    drive    = new Drive();
-    controls = new Controls();
-  }
-
   @Override
-  /**
-   * robotInit()
-   * Runs once when the robot is started
-   */
   public void robotInit() {
-    // Auto start location
-    m_chooser.setDefaultOption("Wall Auto", kWallAuto);
-    m_chooser.addOption("Center Auto", kCenterAuto);
-    m_chooser.addOption("Hangar Auto", kHangarAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-
-    // Auto delay
-    SmartDashboard.putNumber("Auto delay seconds", 0);
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
   }
 
-  @Override
   /**
-   * robotPeriodic()
-   * Always runs on the robot
+   * This function is called every robot packet, no matter the mode. Use this for items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
    */
+  @Override
   public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
   }
 
+  /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  /**
-   * autonomousInit()
-   * Runs once when Auto starts
-   */
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
   public void autonomousInit() {
-    // Choses start position
-    m_autoSelected = m_chooser.getSelected();
-    System.out.println("Auto selected: " + m_autoSelected);
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-    delaySec = (int)SmartDashboard.getNumber("Auto delay seconds", 0);
-
-    // Resets the gyro
-    Drive.ahrs.zeroYaw();
-  }
-
-  @Override
-  /**
-   * autonomousPeriodic()
-   * Runs constantly during Autonomous
-   */
-  public void autonomousPeriodic() {
-    long autoDelayMSec = delaySec * 1000;
-
-    if (status == Robot.CONT) {
-      switch (m_autoSelected) {
-        default:
-          status = DONE;
-          break;
-      }
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
     }
   }
 
+  /** This function is called periodically during autonomous. */
   @Override
-  /**
-   * teleopInit()
-   * Runs once at the start of TeleOp
-   */
+  public void autonomousPeriodic() {}
+
+  @Override
   public void teleopInit() {
-  }
-
-  @Override
-  /**
-   * teleopPeriodic()
-   * Runs constantly during TeleOp
-   */
-  public void teleopPeriodic() {
-    wheelControl();
-  }
-
-  @Override
-  /**
-   * disabledInit()
-   */
-  public void disabledInit() {    
-  }
-
-  @Override
-  /**
-   * disabledPeriodic()
-   * Shouldn't do anything
-   */
-  public void disabledPeriodic() {
-    // Nothing yet...
-  }
-
-  @Override
-  /**
-   * testInit()
-   * Runs once at the start of Test
-   */
-  public void testInit() {
-    // Resets status
-    status = Robot.CONT;
-  }
-
-  @Override
-  /**
-   * testPeriodic()
-   * Runs constantly during test
-   */
-  public void testPeriodic() {
-  }
-
-  /**
-   * Controls the wheels in TeleOp
-   */
-  private void wheelControl() {
-    // Gets Joystick Values
-    double driveX               = controls.getDriveX();
-    double driveY               = controls.getDriveY();
-    double rotatePower          = controls.getRotatePower();
-
-    // Manual driving
-    if (driveMode == DriveMode.MANUAL) {
-      // Drives if we are out of dead zone
-      if ((Math.abs(driveX) > 0.05) ||
-          (Math.abs(driveY) > 0.05) || 
-          (Math.abs(rotatePower) > 0.01)) {
-        drive.teleopSwerve(driveX, driveY, rotatePower, false, true);
-      }
-      else {
-        // Robot is in dead zone, doesn't drive
-        drive.stopWheels();
-      }
-    } 
-
-    // Limelight targeting
-    else {
-      //
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
     }
   }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
 }
-// End of the Robot class
