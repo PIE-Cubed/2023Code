cfa3bbe35ee10bed0ca1e9bd1bfe59e87b40895a
Demeter <robolionsgit@gmail.com>
2023-02-25 15:03:20 -0500

    Merge drive stuff into arm branch

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,7 +6,6 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.networktables.*;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Controls.ArmStates;
